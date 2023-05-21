7b53da130c87c5a0a1cc3cf96e6867f94145b42a
Dana <pythondjango66@gmail.com>
2023-05-21 16:53:01 +0300

    put data in CSV File

--- jumia-scarper.py
+++ jumia-scarper.py
@@ -1,5 +1,5 @@
 from requests_html import HTMLSession
-
+import csv
 
 
 session = HTMLSession()
@@ -13,20 +13,27 @@ url.html.render(sleep=2)
 products = url.html.xpath('/html/body/div[1]/main/div[2]/div[3]/section/div[1]', first=True)
 
 
-for product in products.absolute_links:
-    
-    # print(product.html.find('h3.names', first=True).text)
-    # print(product)
-    
-    url = session.get(product)
-    name = url.html.find('h1.-fs20', first=True).text
-    Product_details = url.html.find('div.markup', first=True).text
-    price = url.html.find('span.-b', first=True).text
-    rate = url.html.find('div.stars', first=True).text
-    image = url.html.find('img.-fw', first=True).text
+# Open the CSV file in write mode
+with open('products.csv', 'w',encoding='utf-8', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['name', 'Product_details', 'price', 'rate', 'image'])
 
-    print(name)
-    print(Product_details)
-    print(price)
-    print(rate)
-    print(image)
+    for product in products.absolute_links:
+        
+        # print(product.html.find('h3.names', first=True).text)
+        # print(product)
+        
+        url = session.get(product)
+        name = url.html.find('h1.-fs20', first=True).text
+        Product_details = url.html.find('div.markup', first=True).text
+        price = url.html.find('span.-b', first=True).text
+        rate = url.html.find('div.stars', first=True).text
+        image = url.html.find('img.-fw', first=True).text
+
+
+        # Write the product information to the CSV file
+        writer.writerow([name, Product_details, price, rate, image])
+
+
+# Output confirmation message
+print("Done")
